d76f9f20acd047b15b2938facddb06027df8c8e2
nazwaaiana <150419868+nazwaaiana@users.noreply.github.com>
2024-12-22 06:35:30 +0700

    Ball.cpp

--- Ball.cpp
+++ Ball.cpp
@@ -6,39 +6,69 @@
 
 class Ball {
 private:
-    sf::CircleShape shape;
-    sf::Vector2f velocity;
-    sf::Vector2f initialPosition; 
+    int id; 
     bool isStriped;
+    bool hit; 
+    sf::Color color; 
+    sf::CircleShape shape; 
+    sf::Vector2f velocity;
+    sf::Vector2f initialPosition;
     sf::Text text;
-    int id;
 
 public:
+    // Constructor
     Ball(float radius, sf::Vector2f position, sf::Color color, int id, const sf::Font& font)
-    : velocity(0.0f, 0.0f), initialPosition(position), isStriped(false), id(id) {
+        : velocity(0.0f, 0.0f), initialPosition(position), isStriped(false), id(id), color(color) { 
 
         if (id >= 9 && id <= 15) {
-            isStriped = true; 
+            isStriped = true;
         }
 
-        shape.setRadius(radius);
-        shape.setFillColor(color);
+        shape.setRadius(radius);  
+        shape.setFillColor(color); 
         shape.setOrigin(radius, radius);
-        shape.setPosition(position + sf::Vector2f(OFFSET_X, OFFSET_Y)); 
+        shape.setPosition(position + sf::Vector2f(OFFSET_X, OFFSET_Y));
         shape.setOutlineThickness(2);
         shape.setOutlineColor(sf::Color::Black);
 
         text.setFont(font);
-        text.setString(std::to_string(id)); 
-        text.setCharacterSize(radius);      
+        text.setString(std::to_string(id));
+        text.setCharacterSize(radius);
         text.setFillColor(sf::Color::Black);
         text.setStyle(sf::Text::Bold);
-        
+
         sf::FloatRect textBounds = text.getLocalBounds();
         text.setOrigin(textBounds.left + textBounds.width / 2, textBounds.top + textBounds.height / 2);
         text.setPosition(shape.getPosition());
     }
 
+    bool isHit() const {
+        return hit;
+    }
+
+    void setHit(bool hitStatus) {
+        hit = hitStatus;
+    }
+
+    bool isCollidingWith(const Ball& other) const {
+        // Menggunakan jarak Euclidean untuk mendeteksi tabrakan
+        float distance = std::sqrt(std::pow(shape.getPosition().x - other.shape.getPosition().x, 2) +
+                                   std::pow(shape.getPosition().y - other.shape.getPosition().y, 2));
+        float radiusSum = shape.getRadius() + other.shape.getRadius();
+        return distance <= radiusSum; // Jika jarak antara dua bola lebih kecil atau sama dengan jumlah radius, berarti tabrakan
+    }
+
+    int getID() const {
+        return id;
+    }
+
+    sf::Color getColor() const {
+        return color;
+    }
+
+    float getRadius() const {
+        return shape.getRadius();  
+    }
 
     void update(float deltaTime) {
         sf::Vector2f newPosition = shape.getPosition() + velocity * deltaTime;
@@ -50,8 +80,8 @@ public:
             velocity.y = -velocity.y;
         }
 
-        float friction = 0.99f; 
-        velocity *= std::pow(friction, deltaTime * 120); 
+        float friction = 0.99f;
+        velocity *= std::pow(friction, deltaTime * 120);
 
         shape.setPosition(shape.getPosition() + velocity * deltaTime);
 
@@ -60,11 +90,11 @@ public:
         }
 
         shape.setPosition(shape.getPosition() + velocity * deltaTime);
-        text.setPosition(shape.getPosition()); // Update posisi teks
+        text.setPosition(shape.getPosition());
     }
 
     void applyForce(sf::Vector2f force) {
-        velocity += force; 
+        velocity += force;
     }
 
     void draw(sf::RenderWindow& window, sf::Font& font) const {
@@ -84,7 +114,7 @@ public:
             number.setString(std::to_string(id));
             number.setCharacterSize(18);
             number.setStyle(sf::Text::Bold);
-            
+
             sf::Vector2f ballPosition = shape.getPosition();
             number.setPosition(ballPosition.x - BallRadius / 2.5f, ballPosition.y - BallRadius / 1.5f);
 
@@ -115,7 +145,17 @@ public:
     }
 
     void respawn() {
-        shape.setPosition(initialPosition + sf::Vector2f(OFFSET_X, OFFSET_Y)); // Apply offset
-        velocity = sf::Vector2f(0.0f, 0.0f); 
+        shape.setPosition(initialPosition + sf::Vector2f(OFFSET_X, OFFSET_Y));
+        velocity = sf::Vector2f(0.0f, 0.0f);
     }
+
+    void setPosition(const sf::Vector2f& newPosition) {
+        shape.setPosition(newPosition + sf::Vector2f(OFFSET_X, OFFSET_Y));
+        text.setPosition(shape.getPosition());
+    }
+
+    bool isMoving() const {
+        return std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y) > MinVelocity;
+    }
+
 };
