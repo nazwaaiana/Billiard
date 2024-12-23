bab0cc4f19e5cd2c93eb9c393f54b4edf6a6d38d
nazwaaiana <150419868+nazwaaiana@users.noreply.github.com>
2024-12-23 12:10:27 +0700

    PoolTable.cpp

--- PoolTable.cpp
+++ PoolTable.cpp
@@ -6,10 +6,14 @@
 class PoolTable {
 private:
     sf::RectangleShape tableShape;
-    RoundedRectangleShape borderShape; // Use RoundedRectangleShape
+    RoundedRectangleShape borderShape; 
     sf::RectangleShape cushionShape;
     std::vector<sf::CircleShape> pockets;
     float pocketRadius;
+
+    sf::Texture borderTexture;
+    sf::Texture cushionTexture;
+    sf::Texture tableTexture;
 
     void setupPockets() {
         std::vector<sf::Vector2f> pocketPositions = {
@@ -20,7 +24,7 @@ private:
             {(WindowWidth / 2) - pocketRadius, WindowHeight - TableBorder - pocketRadius},
             {WindowWidth - TableBorder - pocketRadius, WindowHeight - TableBorder - pocketRadius} 
         };
-    for (const auto& position : pocketPositions) {
+        for (const auto& position : pocketPositions) {
             sf::CircleShape pocket(pocketRadius);
             pocket.setFillColor(sf::Color::Black);
             pocket.setPosition(position + sf::Vector2f(OFFSET_X, OFFSET_Y)); // Apply offset
@@ -30,18 +34,25 @@ private:
 
 public:
     PoolTable() : pocketRadius(25.0f) {
+        if (!borderTexture.loadFromFile("D:/sfmll/bg/border_texture.jpg")) {
+        }
+        if (!cushionTexture.loadFromFile("D:/sfmll/bg/border_texture.jpg")) {
+        }
+        if (!tableTexture.loadFromFile("D:/sfmll/bg/green_texture.jpg")) {
+        }
+
         tableShape.setSize(sf::Vector2f(TableWidth, TableHeight));
-        tableShape.setFillColor(sf::Color(0, 49, 0));
-        tableShape.setPosition(TableBorder + OFFSET_X, TableBorder + OFFSET_Y); // Apply offset
+        tableShape.setTexture(&tableTexture); 
+        tableShape.setPosition(TableBorder + OFFSET_X, TableBorder + OFFSET_Y); 
 
         borderShape.setSize(sf::Vector2f(WindowWidth, WindowHeight));
-        borderShape.setFillColor(sf::Color(40, 119, 61));
-        borderShape.setPosition(OFFSET_X, OFFSET_Y); // Apply offset
-        borderShape.setCornersRadius(20.0f); // Set corner radius
+        borderShape.setTexture(&borderTexture); 
+        borderShape.setPosition(OFFSET_X, OFFSET_Y); 
+        borderShape.setCornersRadius(20.0f); 
 
         cushionShape.setSize(sf::Vector2f(TableWidth + 20, TableHeight + 20));
-        cushionShape.setFillColor(sf::Color(50, 205, 50));
-        cushionShape.setPosition(TableBorder - 10 + OFFSET_X, TableBorder - 10 + OFFSET_Y); // Apply offset
+        cushionShape.setTexture(&cushionTexture); 
+        cushionShape.setPosition(TableBorder - 10 + OFFSET_X, TableBorder - 10 + OFFSET_Y);
 
         setupPockets();
     }
